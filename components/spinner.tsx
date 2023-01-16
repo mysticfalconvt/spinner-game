e8b247d9e151724a6703eb9200665e9172da4524
MysticfalconVT <49008703+mysticfalconvt@users.noreply.github.com>
2023-01-16 15:02:24 -0500

    basic functionality

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,8 +1,26 @@
-import { useState } from "react";
+import React, { ReactNode, useState } from "react";
+
+const options = ["Keep your bet", "lose your bet", "double your bet"];
+const SpinnerSection: React.FC<{
+  name?: string;
+  center?: boolean;
+  active: boolean;
+}> = ({ name, active }): JSX.Element => {
+  return (
+    <div
+      className={`bg-accent text-black text-lg rounded-full flex w-1/2 h-1/2 text-center place-items-center justify-center ${
+        active ? "bg-white" : "bg-accent"
+      }`}
+    >
+      {name}
+    </div>
+  );
+};
 
 export const Spinner = () => {
   const [money, setMoney] = useState(100);
   const [bet, setBet] = useState(50);
+  const [active, setActive] = useState(Math.floor(Math.random() * 8) + 1);
 
   const handleIncreaseBet = () => {
     if (money - bet >= 10) {
@@ -13,6 +31,26 @@ export const Spinner = () => {
     if (bet - 10 >= 10) {
       setBet(bet - 10);
     }
+  };
+
+  const handleSpin = () => {
+    let change = 0;
+    setActive(1);
+    for (let i = 0; i < 100; i++) {
+      setTimeout(() => {
+        const next = Math.floor(Math.random() * 8) + 1;
+        setActive(next);
+      }, i * 1);
+    }
+    if (active === 1 || active === 4 || active === 7) {
+    }
+    if (active === 2 || active === 5 || active === 8) {
+      change = -bet;
+    }
+    if (active === 3 || active === 6) {
+      change = bet * 2;
+    }
+    setMoney(money + change);
   };
 
   return (
@@ -35,7 +73,28 @@ export const Spinner = () => {
           increase bet
         </button>
       </div>
-      <div className="bg-accent aspect-square rounded-xl">asd</div>
+      <div className="bg-secondary aspect-square rounded-full grid grid-cols-3 w-1/2 items-center place-items-center">
+        <SpinnerSection name={options[0]} active={active === 1} />
+        <SpinnerSection name={options[1]} active={active === 2} />
+        <SpinnerSection name={options[2]} active={active === 3} />
+        <SpinnerSection name={options[0]} active={active === 4} />
+        {money > 0 ? (
+          <div
+            className="bg-base-200 text-white rounded-full flex w-1/2 h-1/2 text-center place-items-center justify-center"
+            onClick={handleSpin}
+          >
+            SPIN!!!
+          </div>
+        ) : (
+          <div className="bg-red-900  text-white  rounded-full flex w-1/2 h-1/2 text-center place-items-center justify-center">
+            You are out of money
+          </div>
+        )}
+        <SpinnerSection name={options[1]} active={active === 5} />
+        <SpinnerSection name={options[2]} active={active === 6} />
+        <SpinnerSection name={options[0]} active={active === 7} />
+        <SpinnerSection name={options[1]} active={active === 8} />
+      </div>
     </div>
   );
 };
